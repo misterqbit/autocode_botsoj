44e186768f804937f61a623cbb9da1d8f8d61e01
misterqbit <53399858+misterqbit@users.noreply.github.com>
2021-12-18 18:23:22 +0100

    Create review_search.js

---
+++ review_search.js
@@ -0,0 +1,136 @@
+// authenticates you with the API standard library
+// type `await lib.` to display API autocomplete
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+let game = context.params.event.data.options[0].options[0].value;
+let magazine = context.params.event.data.options[0].name;
+
+let keyword = game.toLowerCase();
+let keywords = keyword.split(' ');
+
+if (magazine == 'tilt'){
+  let result = await lib.googlesheets.query['@0.3.0'].select({
+        range: `cateltilt!A:J`,
+        bounds: 'FIRST_EMPTY_ROW',
+        where: [
+          {
+          'lowercasetitre__contains' : `${keywords[0]}`
+          }
+        ],
+        limit: {
+          'count': 0,
+          'offset': 0
+        },
+  });
+
+  //refining search
+  if (keywords.length > 0) {
+    for (let i = 1; i < keywords.length; i++) {
+      for (let j = 0; j < result.rows.length; j++) {
+        if (result.rows[j].fields.lowercasetitre.includes(keywords[i])) {
+        }
+        else {
+          result.rows.splice(j,1);
+          j=0;
+        }
+      }
+    }
+  }
+  //end of refining search  
+
+  if (result.rows[0] == null) {
+        await lib.discord.channels['@0.0.6'].messages.create({
+          channel_id: context.params.event.channel_id,
+          content: `@${context.params.event.member.user.username} : désolé, pas de résultat pour ${game}.`
+        });
+  } 
+  else if (result.rows.length > 10) {
+    await lib.discord.channels['@0.0.6'].messages.create({
+      channel_id: context.params.event.channel_id,
+      content: `@${context.params.event.member.user.username} : trop de résultats pour ${game}, veuillez affiner votre recherche, merci.`
+    });
+  }
+  else {
+      let recherche = [];
+      let urlTilt;
+      for (let i = 0; i < result.rows.length; i++) {
+          recherche.push(`\n${result.rows[i].fields.Titre}`);
+          recherche.push(` sur ${result.rows[i].fields.Machine}`); 
+          recherche.push(` dans un article du Tilt n° ${result.rows[i].fields.Numero}`); 
+          recherche.push(` en ${result.rows[i].fields.Dates}`);
+          urlTilt = encodeURI(result.rows[i].fields.Lien); 
+          recherche.push(` ${urlTilt}`); 
+      } 
+      await lib.discord.channels['@0.0.6'].messages.create({
+            channel_id: `${context.params.event.channel_id}`,
+            content: [
+            `<@!${context.params.event.member.user.id}> : vous trouverez ${game} dans:`,
+            `${recherche}`,
+            '___'
+            ].join('\n')
+      });
+  }
+}
+else {
+  let result = await lib.googlesheets.query['@0.3.0'].select({
+        range: `catelgen4!A:J`,
+        bounds: 'FIRST_EMPTY_ROW',
+        where: [
+          {
+          'lowercasetitre__contains' : `${keywords[0]}`
+          }
+        ],
+        limit: {
+          'count': 0,
+          'offset': 0
+        },
+  });
+
+  //refining search
+  if (keywords.length > 0) {
+    for (let i = 1; i < keywords.length; i++) {
+      for (let j = 0; j < result.rows.length; j++) {
+        if (result.rows[j].fields.lowercasetitre.includes(keywords[i])) {
+        }
+        else {
+          result.rows.splice(j,1);
+          j=0;
+        }
+      }
+    }
+  }
+  //end of refining search  
+
+  if (result.rows[0] == null) {
+        await lib.discord.channels['@0.0.6'].messages.create({
+          channel_id: context.params.event.channel_id,
+          content: `@${context.params.event.member.user.username} : désolé, pas de résultat pour ${game}.`
+        });
+  } 
+  else if (result.rows.length > 10) {
+    await lib.discord.channels['@0.0.6'].messages.create({
+      channel_id: context.params.event.channel_id,
+      content: `@${context.params.event.member.user.username} : trop de résultats pour ${game}, veuillez affiner votre recherche, merci.`
+    });
+  }
+  else {
+      let recherche = [];
+      let urlGen4;
+      for (let i = 0; i < result.rows.length; i++) {
+          recherche.push(`\n${result.rows[i].fields.Titre}`);
+          recherche.push(` sur ${result.rows[i].fields.Machine}`); 
+          recherche.push(` dans un article du Génération 4 n° ${result.rows[i].fields.Numero}`); 
+          recherche.push(` en ${result.rows[i].fields.Dates}`);
+          urlGen4 = encodeURI(result.rows[i].fields.Lien); 
+          recherche.push(` ${urlGen4}`); 
+      } 
+      await lib.discord.channels['@0.0.6'].messages.create({
+            channel_id: `${context.params.event.channel_id}`,
+            content: [
+            `<@!${context.params.event.member.user.id}> : vous trouverez ${game} dans:`,
+            `${recherche}`,
+            '___'
+            ].join('\n')
+      });
+  }
+}
