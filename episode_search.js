a44b458e7f63524524d173368ab67e73943ce3f5
misterqbit <53399858+misterqbit@users.noreply.github.com>
2021-12-23 14:02:27 +0100

    Update episode_search.js

--- episode_search.js
+++ episode_search.js
@@ -22,21 +22,27 @@ let result = await lib.googlesheets.query['@0.3.0'].select({
 if (result.rows[0] == null) {
       await lib.discord.channels['@0.0.6'].messages.create({
         channel_id: context.params.event.channel_id,
-        content: `<@!${context.params.event.member.user.id}> : pas de résultat`
+        content: `<@!${context.params.event.member.user.id}> : pas de résultat pour "${search}".`
       });
 } 
+else if (result.rows.length > 9) {
+  await lib.discord.channels['@0.0.6'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `@${context.params.event.member.user.username} : trop de résultats pour "${search}", veuillez affiner votre recherche, merci.`
+  });
+}
 else {
     let recherche = [];
     for (let i = 0; i < result.rows.length; i++) {
             recherche.push(`\n${result.rows[i].fields.themes}`); 
-            recherche.push(`Date: ${result.rows[i].fields.date}`);  
-            //recherche.push(`Saison: ${result.rows[i].fields.saison}`);
+            recherche.push(` ${result.rows[i].fields.date}`);  
+            recherche.push(` ${result.rows[i].fields.weburl}`);
             //recherche.push(`Ep.: ${result.rows[i].fields.episode}`);
     }   
     await lib.discord.channels['@0.0.6'].messages.create({
           channel_id: `${context.params.event.channel_id}`,
           content: [
-          `<@!${context.params.event.member.user.id}> : vous trouverez ${keyword} dans:`,
+          `<@!${context.params.event.member.user.id}> : vous trouverez "${search}" dans:`,
           `${recherche}`,
           '___'
           ].join('\n')
