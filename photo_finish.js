867021df2cbb151e16af773b54dbac3d0ebe415e
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-01-06 13:12:14 +0100

    Create photo_finish.js

---
+++ photo_finish.js
@@ -0,0 +1,24 @@
+//displays exact message timestamp
+
+// authenticates you with the API standard library
+// type `await lib.` to display API autocomplete
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+let messageID = context.params.event.data.options[0].value;
+console.log(messageID);
+
+let result = await lib.discord.channels['@0.0.6'].messages.retrieve({
+  message_id: `${context.params.event.data.options[0].value}`,
+  channel_id: `${context.params.event.channel_id}`
+});
+
+await lib.discord.channels['@0.0.6'].messages.create({
+  channel_id: context.params.event.channel_id,
+  content: [
+    `<@${context.params.event.member.user.id}> :`,
+//    `>>> Message ID : ${messageID}`,
+    `>>> Le message "${result.content}" de ${result.author.username} a été posté exactement à ${result.timestamp}`,
+  ].join('\n'),
+  tts : true
+  
+});
