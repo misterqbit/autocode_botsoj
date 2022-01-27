dba6d1c6353e7f819f8392124fc8cc26e254f0b7
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-01-27 09:42:39 +0100

    Update lost_in_cpc.js

--- lost_in_cpc.js
+++ lost_in_cpc.js
@@ -2,6 +2,29 @@
 // type `await lib.` to display API autocomplete
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
+await lib.discord.interactions['@0.0.0'].followups.ephemeral.create({
+  token: `${context.params.event.token}`,
+  content: `Bravo! Vous avez trouvé tous les secrets de ce voyage dans les univers de SRAM, mais aussi de...`,
+  embeds: [
+    {
+      "type": "rich",
+      "title": "",
+      "description": "",
+      "color": 0x00FFFF,
+      "image": {
+        "url": `https://i.ibb.co/b2WC5D4/thend.gif`,
+        "height": 0,
+        "width": 400
+      }
+    }
+  ]
+});
+
+await lib.discord.interactions['@0.0.0'].followups.ephemeral.create({
+  token: `${context.params.event.token}`,
+  content: `La fin? Quelle fin? M'enfin! Je veux la trouver cette fin! Je veux explorer, résoudre des énigmes, éviter les pièges, améliorer mes compétences, je veux la mériter, cette fin! Cachez cette fin que je ne saurais voir! Et démarrons cette aventure`,
+});
+
 await lib.discord.channels['@0.2.0'].messages.create({
   "channel_id": `${context.params.event.channel_id}`,
   "content": `>DEPART:`,
