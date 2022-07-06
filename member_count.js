e91e72dd668e0f113e3bd33a8c951ad0d02c2f76
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-07-06 12:30:05 +0200

    Update member_count.js

--- member_count.js
+++ member_count.js
@@ -12,3 +12,52 @@ let messageResponse = await lib.discord.channels['@0.0.6'].messages.create({
   `<@!${context.params.event.member.user.id}> : nous sommes **${result.approximate_member_count}** membres dont **${result.approximate_presence_count}** en ligne!`
 });
 return messageResponse;
+
+
+
+// authenticates you with the API standard library
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+// make API request
+let result = await lib.discord.guilds['@0.0.6'].preview.list({
+  guild_id: `${context.params.event.guild_id}`
+});
+
+// removing 3 robots
+let members_number = result.approximate_member_count - 3;
+let active_members_number = result.approximate_presence_count - 3;
+members_number = members_number.toString();
+/////
+
+///bonus message START
+let bonus_message = '';
+
+let population = await lib.googlesheets.query['@0.3.0'].select({
+      range: `insee2019!A:C`,
+      bounds: 'FIRST_EMPTY_ROW',
+      where: [
+        {
+        'pop__contains' : `${members_number}`
+                  }
+      ],
+      limit: {
+        'count': 0,
+        'offset': 0
+      },
+});
+/*console.log(population.rows[0]);
+console.log(members_number);*/
+if (population.rows[0] != null) {
+  let commune = population.rows[0].fields.comm;
+  let departement = population.rows[0].fields.dep;
+  bonus_message = ` Soit autant que la population de ${commune}, dans le ${departement}`;
+}
+///bonus message END
+
+
+let messageResponse = await lib.discord.channels['@0.0.6'].messages.create({
+  channel_id: `${context.params.event.channel_id}`,
+  content: 
+  `<@!${context.params.event.member.user.id}> : nous sommes **${members_number}** membres dont **${active_members_number}** en ligne! Et 3 robots.${bonus_message}`
+});
+return messageResponse;
