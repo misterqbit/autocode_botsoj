080587ba13eaa33111a7d8440ef8bd2814320da5
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-08-26 18:54:52 +0200

    Update deaf_test_cpc.button.interaction.js

--- deaf_test_cpc.button.interaction.js
+++ deaf_test_cpc.button.interaction.js
@@ -2,6 +2,12 @@
 // type `await lib.` to display API autocomplete
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 let result = await lib.googlesheets.query['@0.3.0'].select({
   range: `variables!B:C`,
   bounds: 'FIRST_EMPTY_ROW',
@@ -16,11 +22,11 @@ let result = await lib.googlesheets.query['@0.3.0'].select({
   },
 });
 
-if (result.rows[0].fields.content != 0) {
+/*if (result.rows[0].fields.content != 0) {*/
   
   await lib.discord.channels['@0.2.0'].messages.create({
     "channel_id": `${context.params.event.channel_id}`,
-    "content": ` `,
+    "content": ` https://www.cpc-power.com/index.php?page=detail&num=${result.rows[0].fields.content}`,
     "tts": false,
     "embeds": [
       {
@@ -36,7 +42,8 @@ if (result.rows[0].fields.content != 0) {
       }
     ]
   });
-  await lib.googlesheets.query['@0.3.0'].update({
+//  console.log(`https://www.cpc-power.com/extra_lire_fichier.php?extra=cpcold&fiche=${result.rows[0].fields.content}&slot=1&part=A&type=.png`);
+/*  await lib.googlesheets.query['@0.3.0'].update({
     range: `variables!B:C`,
     bounds: 'FIRST_EMPTY_ROW',
     where: [
@@ -51,6 +58,59 @@ if (result.rows[0].fields.content != 0) {
     fields: {
         'content': 0
     }
+  });*/
+  let randomgamenumber = getRandomInt(1,3698);
+  
+  await lib.googlesheets.query['@0.3.0'].update({
+      range: `variables!B:C`,
+      bounds: 'FIRST_EMPTY_ROW',
+      where: [
+        {
+          'name': `cpc_game_id`,
+        }
+      ],
+      limit: {
+        'count': 0,
+        'offset': 0
+      },
+      fields: {
+          'content': `${randomgamenumber}`
+      }
   });
-}
-else {}
+  
+  //display quiz message
+  await lib.discord.channels['@0.2.0'].messages.create({
+      "channel_id": `${context.params.event.channel_id}`,
+      "content": ` `,
+      "tts": false,
+      "components": [
+        {
+          "type": 1,
+          "components": [
+            {
+              "style": 3,
+              "label": `Solution`,
+              "custom_id": `cpc_solution_button_id`,
+              "disabled": false,
+              "type": 2
+            }
+          ]
+        }
+      ],
+      "embeds": [
+        {
+          "type": "rich",
+          "title": "",
+          "description": "",
+          "color": 0x00FFFF,
+          "image": {
+            "url": `https://www.cpc-power.com/extra_lire_fichier.php?extra=cpcold&fiche=${randomgamenumber}&slot=2&part=A&type=.png`,
+            "height": 0,
+            "width": 0
+          }
+        }
+      ]
+  });
+
+/*}
+else {}*/
