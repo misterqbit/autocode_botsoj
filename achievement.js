e5b2c97d5eff4f796fddd5ace3909472ecb34f21
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-10-02 18:04:23 +0200

    Update achievement.js

--- achievement.js
+++ achievement.js
@@ -2,19 +2,26 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const jimp = require('jimp');
 
 let text = context.params.event.data.options[0].value.toString();
-const maxlength = 37;
+const maxlength1 = 17;
+const maxlength2 = 37;
 
 console.log(text);
-if (text.length > maxlength) {
-    text = text.substring(0,maxlength);
+if (text.length > maxlength2) {
+    text = text.substring(0,maxlength2);
     text = text.concat('...');
+    var font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
+}
+else if ((text.length <= maxlength2)&&(text.length > maxlength1)) {
+    var font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
+}
+else {
+    var font = await jimp.loadFont(jimp.FONT_SANS_32_WHITE);
 }
 
 let img = await jimp.read(
     'https://media.discordapp.net/attachments/642122837448065054/1026088395224666182/toppng.com-xbox-achievement-unlocked-png-xbox-360-471x87.png'
 );
 
-const font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
 img.print(font, 105, 50, `${text}`);
 
 let buffer = await img.getBufferAsync(jimp.MIME_PNG);
