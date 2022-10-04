8ce8c25b6112356f95affbc0971a10738eaefe9d
misterqbit <53399858+misterqbit@users.noreply.github.com>
2022-10-04 14:33:11 +0200

    Update achievement.js

--- achievement.js
+++ achievement.js
@@ -2,27 +2,49 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 const jimp = require('jimp');
 
 let text = context.params.event.data.options[0].value.toString();
-const maxlength1 = 17;
-const maxlength2 = 37;
+let font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
+
+const maxlength1 = 37;
+const maxlength2 = 74;
+
+
+let img = await jimp.read(
+    'https://media.discordapp.net/attachments/1026168814712406037/1026168900473344020/toppng.com-xbox-achievement-unlocked-png-xbox-360-471x87.png'
+);
 
 console.log(text);
 if (text.length > maxlength2) {
-    text = text.substring(0,maxlength2);
-    text = text.concat('...');
-    var font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
+    let text1 = text.substring(0,maxlength1);
+    text1 = text1.substr(0,Math.min(maxlength1, text1.lastIndexOf(" ")));
+    if (text1.length == 0) {
+      text1 = text.substring(0,maxlength1);
+    }
+    let text2 = text.substring(text1.length,maxlength2);
+    text2 = text2.substring(0,maxlength1);
+    text2 = text2.concat('...');
+    img.print(font, 105, 40, `${text1}`);
+    img.print(font, 105, 60, `${text2}`);
 }
-else if ((text.length <= maxlength2)&&(text.length > maxlength1)) {
-    var font = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
+
+else if ((text.length > maxlength1)&&(text.length <= maxlength2)) {
+    let text1 = text.substring(0,maxlength1);
+    text1 = text1.substr(0,Math.min(maxlength1, text1.lastIndexOf(" ")));
+    if (text1.length == 0) {
+        text1 = text.substring(0,maxlength1);
+    }
+    let text2 = text.substring(text1.length,maxlength2);
+
+    if (text2.length > maxlength1) {
+      text2 = text2.substring(0,maxlength1);
+      text2 = text2.concat('...');
+    }
+    img.print(font, 105, 40, `${text1}`);
+    img.print(font, 105, 60, `${text2}`);
 }
+
 else {
-    var font = await jimp.loadFont(jimp.FONT_SANS_32_WHITE);
+    img.print(font, 105, 50, `${text}`);
 }
-
-let img = await jimp.read(
-    'https://media.discordapp.net/attachments/642122837448065054/1026088395224666182/toppng.com-xbox-achievement-unlocked-png-xbox-360-471x87.png'
-);
-
-img.print(font, 105, 50, `${text}`);
 
 let buffer = await img.getBufferAsync(jimp.MIME_PNG);
 
